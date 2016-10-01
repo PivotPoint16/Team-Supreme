09d3b9951e01f84aef33b2e07ccac103810e4f8b
Steven-Roberts <steven94@vt.edu>
2016-10-01 13:34:36 -0400

    Improve routing

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 
+const siteMetadata = {
+    title: 'Spring Board'
+};
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Spring Board'
-    });
+    res.render('index', siteMetadata);
+});
+
+app.get('/:page', (req, res) => {
+    res.render(req.params.page, siteMetadata);
 });
 
 const port = 3000;
